8ed439d2351994f84765ae0ee7f3ebed51198032
Ervy <ervy@sfs-wifi-dhcp-10-143-235-16.wifi.sfsu.edu>
2019-02-06 19:22:18 -0800

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Ervy on 2/6/19.
+//  Copyright © 2019 Angelo m. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
